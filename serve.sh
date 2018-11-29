26f4ff8869eecf87becc2344cc93567749a6393a
kisaku1978 <kisaku1908@outlook.com>
2018-11-29 05:13:23 +0000

    update jekyll files.

---
+++ serve.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+PATH=/root/.rbenv/shims:/usr/local/rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+ip=$(ifconfig eth0 | grep inet | awk '{print $2}' | sed 's/addr://')
+port=4444
+bundle exec jekyll serve --host ${ip} --port ${port}
